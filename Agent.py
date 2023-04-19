b9083c95e295d2fdd63e58930c6bcbd2ffdf27a7
Killias <vuducquynh1182002@gmail.com>
2023-04-19 13:42:35 +0700

    update bot

--- Agent.py
+++ Agent.py
@@ -33,10 +33,14 @@ def Test(state, per):
     ValidActions = np.where(ValidActions==1)[0]
 
 
-    if state[12] <= 8 and 1 in ValidActions:
-        return 1, per
+    if state[12] <= 9 and 1 in ValidActions:
+        return 1, perData
     if state[13] >= 4 and 0 in ValidActions:
-        return 0, per
+        return 0, perData
+
+    for i in range(3,15):
+        if i in ValidActions:
+            return i, per
     
     action = ValidActions[np.random.randint(len(ValidActions))]
-    return action, per
+    return action, perData
